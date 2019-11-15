9ae815494781d5404a23366f432f77eb20b27c71
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-11-16 01:53:48 +0500

    add user info on `menu` screen
    info block contains user name, surname, street, building, apartment
    
    done #13
    
    Signed-off-by: dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>

--- app/src/main/java/com/dvor/my/mydvor/menu/MenuFragment.kt
+++ app/src/main/java/com/dvor/my/mydvor/menu/MenuFragment.kt
@@ -1,26 +1,38 @@
 package com.dvor.my.mydvor.menu
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.dvor.my.mydvor.R
+import com.dvor.my.mydvor.data.User
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MenuFragment : Fragment() {
 
+    lateinit var mAuth: FirebaseAuth
+    lateinit var database: DatabaseReference
+    lateinit var user: User
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_menu, container, false)
-        val mAuth = FirebaseAuth.getInstance()
+        mAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+        // variable init
+        user = User("", "", "", "", "", "", "")
+        setUserListener()
 
         view.findViewById<Button>(R.id.ib_news).setOnClickListener { goto(newsFragmentId) }
         view.findViewById<Button>(R.id.ib_stock).setOnClickListener { goto(stockFragmentId) }
         view.findViewById<Button>(R.id.ib_message).setOnClickListener { goto(messageFragmentId) }
         view.findViewById<Button>(R.id.ib_notification).setOnClickListener { goto(notificationFragmentId) }
-//        view.findViewById<Button>(R.id.ib_service).setOnClickListener { goto(serviceFragmentId) }
+        view.findViewById<Button>(R.id.ib_service).setOnClickListener { goto(serviceFragmentId) }
 
         view.findViewById<Button>(R.id.ib_logout).setOnClickListener { mAuth.signOut() }
 
@@ -44,6 +56,76 @@ class MenuFragment : Fragment() {
                 .replace(R.id.menuFragment, nextFragment) // nextFragment из аргумента функции
                 .addToBackStack(null)
                 .commit()*/
+    }
+
+    /**
+     * Updates view, set `username` and `address` textviews
+     */
+    private fun updateUI() {
+        view?.findViewById<TextView>(R.id.username)?.text = getUsername()
+        view?.findViewById<TextView>(R.id.address)?.text = getAddress()
+    }
+
+    /**
+     * Returns name and surname
+     */
+    private fun getUsername(): String = "${user.name} ${user.surname}"
+
+    /**
+     * Returns address, where user live
+     */
+    private fun getAddress(): String = "${user.street} ${user.building} кв. ${user.apartment}"
+
+    /**
+     * Sets database listener for fetch data from `users` branch.
+     *
+     * Other branches are not implemented
+     */
+    private fun setUserListener() {
+        val listener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                user.name = dataSnapshot.child("name").value.toString()
+                user.surname = dataSnapshot.child("surname").value.toString()
+                user.street_id = dataSnapshot.child("street_id").value.toString()
+                user.building_id = dataSnapshot.child("building_id").value.toString()
+                user.apartment = dataSnapshot.child("apartment").value.toString()
+
+                setAddressListener()
+
+                updateUI()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("state", databaseError.message)
+            }
+        }
+
+        val userBranch = database.child("users").child(mAuth.uid.toString())
+        userBranch.addValueEventListener(listener)
+    }
+
+    /**
+     * Sets database listener for fetch data from `streets` branch
+     */
+    private fun setAddressListener() {
+        val listener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                user.street = dataSnapshot.child("name").value.toString()
+                user.building = dataSnapshot
+                        .child("buildings")
+                        .child(user.building_id.toString())
+                        .child("number").value.toString()
+
+                updateUI()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("state", databaseError.message)
+            }
+        }
+
+        val streetsBranch = database.child("streets").child(user.street_id.toString())
+        streetsBranch.addValueEventListener(listener)
     }
 
     companion object {
