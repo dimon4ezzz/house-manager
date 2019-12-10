73e8128c662d6eb3ff853c8aeade7ad0d6e7aabb
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-10 22:43:10 +0500

    made variables private

--- app/src/main/java/com/dvor/my/mydvor/menu/MenuFragment.kt
+++ app/src/main/java/com/dvor/my/mydvor/menu/MenuFragment.kt
@@ -22,11 +22,11 @@ class MenuFragment : Fragment() {
     lateinit var database: DatabaseReference
     lateinit var user: User
 
-    lateinit var userBranch: DatabaseReference
-    lateinit var streetBranch: DatabaseReference
+    private lateinit var userBranch: DatabaseReference
+    private lateinit var streetBranch: DatabaseReference
 
-    lateinit var userListener: ValueEventListener
-    lateinit var streetListener: ValueEventListener
+    private lateinit var userListener: ValueEventListener
+    private lateinit var streetListener: ValueEventListener
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_menu, container, false)
