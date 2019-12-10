accc5a71128bf510a0abd807aa3d80a2e099cce9
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-10 13:48:41 +0500

    removed unused import

--- app/src/main/java/com/dvor/my/mydvor/MessageFakeActivity.kt
+++ app/src/main/java/com/dvor/my/mydvor/MessageFakeActivity.kt
@@ -3,7 +3,6 @@ package com.dvor.my.mydvor
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dvor.my.mydvor.MainActivity
 
 class MessageFakeActivity : AppCompatActivity() {
 
