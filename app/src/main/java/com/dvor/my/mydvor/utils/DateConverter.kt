5f58aca1aa9424c4cc072cd1aec1e523e19b49ec
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-10 22:33:30 +0500

    added support date format `dd.MM.yy HH:mm:ss`
    sample: `31.12.19 23:59:59`

--- app/src/main/java/com/dvor/my/mydvor/utils/DateConverter.kt
+++ app/src/main/java/com/dvor/my/mydvor/utils/DateConverter.kt
@@ -2,12 +2,36 @@ package com.dvor.my.mydvor.utils
 
 import android.icu.text.SimpleDateFormat
 import android.text.format.DateUtils
+import java.text.ParseException
 import java.util.*
 
+/**
+ * Small date converter.
+ */
 object DateConverter {
+    @Deprecated("don't use old date format")
+    private const val oldFormat = "EEE MMM dd HH:mm:ss zzzz yyyy"
+    private const val format = "dd.MM.yy HH:mm:ss"
+
+    /**
+     * Converts usual date in string to relative date in string.
+     *
+     * @param date date in string, please use format: `dd.MM.yy HH:mm:ss`
+     * @sample convert("31.12.19 23:59:59") become `tomorrow`, or `today`, or `Dec 31, 19`
+     * @throws Exception when it cannot convert date
+     */
     fun convert(date: String): String {
-        val format = SimpleDateFormat("EEE MMM dd HH:mm:ss zzzz yyyy", Locale.ENGLISH)
-        val parsedDate = format.parse(date)
+        val parsedDate: Date
+
+        parsedDate = try {
+            SimpleDateFormat(oldFormat, Locale.ENGLISH)
+                    .parse(date)
+        } catch (e: ParseException) {
+            SimpleDateFormat(format)
+                    .parse(date)
+        } catch (e: Exception) {
+            throw e
+        }
 
         return DateUtils.getRelativeTimeSpanString(parsedDate.time).toString()
     }
