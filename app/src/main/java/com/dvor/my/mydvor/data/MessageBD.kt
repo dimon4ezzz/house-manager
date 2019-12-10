791237da156f7349b55c765da0767f1fab23c7a2
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-10 15:12:34 +0500

    set Keep annotation for database POJO
    fixed #35

--- app/src/main/java/com/dvor/my/mydvor/data/MessageBD.kt
+++ app/src/main/java/com/dvor/my/mydvor/data/MessageBD.kt
@@ -1,8 +1,10 @@
 package com.dvor.my.mydvor.data
 
+import androidx.annotation.Keep
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
+@Keep
 data class MessageBD(
         var income: Int?,
         var text: String?,
--- app/src/main/java/com/dvor/my/mydvor/data/NewsBD.kt
+++ app/src/main/java/com/dvor/my/mydvor/data/NewsBD.kt
@@ -1,8 +1,10 @@
 package com.dvor.my.mydvor.data
 
+import androidx.annotation.Keep
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
+@Keep
 data class NewsBD(
         var header: String?,
         var text: String?,
