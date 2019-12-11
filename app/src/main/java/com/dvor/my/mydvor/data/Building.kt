3115f621e6b55376480757a1b9f6d3a0f820018f
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-11 22:36:06 +0500

    Street and Building toString override for Registration form

--- app/src/main/java/com/dvor/my/mydvor/data/Building.kt
+++ app/src/main/java/com/dvor/my/mydvor/data/Building.kt
@@ -3,4 +3,10 @@ package com.dvor.my.mydvor.data
 data class Building(
         val id: String,
         var number: String
-)
+) {
+    /**
+     * Must be override, because Registration use `toString` method
+     */
+    override fun toString(): String =
+            number
+}
--- app/src/main/java/com/dvor/my/mydvor/data/Street.kt
+++ app/src/main/java/com/dvor/my/mydvor/data/Street.kt
@@ -3,4 +3,10 @@ package com.dvor.my.mydvor.data
 data class Street(
         val id: String = "",
         var name: String = ""
-)
+) {
+    /**
+     * Must be override, because Registration use `toString` method
+     */
+    override fun toString(): String =
+            name
+}
