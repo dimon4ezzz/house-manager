174a2d5470d7b1556188c2f9cc4498433f595a46
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-12 23:17:37 +0500

    implemented settings w/o spinners and later email/password update

--- app/src/main/java/com/dvor/my/mydvor/settings/SettingsFragment.kt
+++ app/src/main/java/com/dvor/my/mydvor/settings/SettingsFragment.kt
@@ -1,23 +1,289 @@
 package com.dvor.my.mydvor.settings
 
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.dvor.my.mydvor.R
+import com.dvor.my.mydvor.data.Building
+import com.dvor.my.mydvor.data.Street
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 /**
  * A simple [Fragment] subclass.
  */
 class SettingsFragment : Fragment() {
 
+    private lateinit var fullName: EditText
+    private lateinit var streets: Spinner
+    private lateinit var buildings: Spinner
+    private lateinit var apartment: EditText
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var deleter: Button
+
+    private val mAuth = FirebaseAuth.getInstance()
+
+    private val database = FirebaseDatabase.getInstance().reference
+    private var usersBranch = database.child("users")
+    private var streetsBranch = database.child("streets")
+    private var buildingsBranch = database.child("buildings")
+
+    private var usersBranchListener: ValueEventListener? = null
+    private var streetsBranchListener: ValueEventListener? = null
+    private var buildingsBranchListener: ValueEventListener? = null
+
+    private var streetsList: List<Street>? = null
+    private var buildingsList: List<Building>? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        fullName = view.findViewById(R.id.et_name)
+        fullName.onSubmit {
+            checkAndSendFullName()
+        }
+
+        streets = view.findViewById(R.id.s_update_street)
+        buildings = view.findViewById(R.id.s_update_building)
+        apartment = view.findViewById(R.id.et_update_apartment)
+
+        email = view.findViewById(R.id.update_email)
+        email.onSubmit {
+            checkAndSendEmail()
+        }
+
+        password = view.findViewById(R.id.update_password)
+        password.onSubmit {
+            checkAndSendPassword()
+        }
+
+        deleter = view.findViewById(R.id.delete)
+        deleter.setOnClickListener {
+            deleteUser()
+        }
+
+        streetsSpinnerInit()
+
+        return view
     }
 
+    private fun checkAndSendFullName() {
+        if (fullName.text.toString().isBlank())
+            fullName.error = resources.getString(R.string.empty_full_name)
 
+        val arr = fullName.text.toString().split(" ")
+        if (arr.count() < 2)
+            fullName.error = resources.getString(R.string.not_full_name)
+
+        database.child("users")
+                .child(mAuth.currentUser!!.uid)
+                .child("name")
+                .setValue(arr[0])
+
+        database.child("users")
+                .child(mAuth.currentUser!!.uid)
+                .child("surname")
+                .setValue(arr[1])
+
+        Toast.makeText(requireContext(), R.string.successful_name_changing, Toast.LENGTH_LONG).show()
+    }
+
+    /* TODO сюда нужно добавить разлогин и новый логин,
+        потому что операция обновления email требует недавнего входа
+    */
+    private fun checkAndSendEmail() {
+        if (email.text.toString().isBlank())
+            email.error = resources.getString(R.string.empty_email)
+
+        mAuth.currentUser!!.updateEmail(email.text.toString())
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), R.string.successful_email_change, Toast.LENGTH_LONG).show()
+                }.addOnFailureListener {
+                    Log.d("state", it.message.toString())
+                    Toast.makeText(requireContext(), R.string.unsuccessful_email_changing, Toast.LENGTH_LONG).show()
+                }
+    }
+
+    /* TODO сюда нужно добавить разлогин и новый логин,
+        потому что операция обновления пароля требует недавнего входа
+    */
+    private fun checkAndSendPassword() {
+        if (password.text.toString().isBlank())
+            password.error = resources.getString(R.string.empty_password)
+
+        mAuth.currentUser!!.updatePassword(password.text.toString())
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), R.string.successful_email_change, Toast.LENGTH_LONG).show()
+                }.addOnFailureListener {
+                    Log.d("state", it.message.toString())
+                    Toast.makeText(requireContext(), R.string.unsuccessful_email_changing, Toast.LENGTH_LONG).show()
+                }
+    }
+
+    private fun deleteUser() {
+        val builder = AlertDialog.Builder(activity)
+        builder.setMessage("Действительно ли вы хотите удалить аккаунт ${mAuth.currentUser!!.email}? Это не удалит ваши данные в базе данных, но зайти вы больше не сможете!")
+                .setTitle(resources.getString(R.string.account_deletion))
+
+        builder.setPositiveButton(R.string.delete) { _, _ -> realDelete() }
+        builder.setNegativeButton(R.string.discard) { a, _ -> a.dismiss() }
+
+        builder.create().show()
+    }
+
+    private fun realDelete() {
+        mAuth.currentUser!!.delete()
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(), R.string.unsuccessful_account_deletion, Toast.LENGTH_LONG).show()
+                }
+    }
+
+    /**
+     * When user press `enter` key in text field,
+     * calls `f` function.
+     *
+     * @param f function to call; e.g. sendData
+     * @sample onSubmit { sendData() }
+     */
+    private fun EditText.onSubmit(f: () -> Unit) {
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                f()
+            }
+
+            true
+        }
+    }
+
+    private fun streetsSpinnerInit() {
+        streetsBranchListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("state", p0.message)
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                streetsList = p0.children.map {
+                    Street(
+                            id = it.key.toString(),
+                            name = it.child("name").value.toString()
+                    )
+                }
+
+                setStreetsSpinnerAdapter()
+            }
+        }
+
+        streetsBranch.addValueEventListener(streetsBranchListener!!)
+
+        setStreetSpinnerAction()
+    }
+
+    private fun setStreetsSpinnerAdapter() {
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, streetsList!!)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapter.notifyDataSetChanged()
+        streets.adapter = adapter
+    }
+
+    private fun setStreetSpinnerAction() {
+        streets.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Log.d("state", "nothing was selected in streets")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                buildingsSpinnerInit()
+            }
+
+        }
+    }
+
+    private fun buildingsSpinnerInit() {
+        buildingsBranchListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("state", p0.message)
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                buildingsList = p0.children.map {
+                    Building(
+                            id = it.key.toString(),
+                            number = it.child("number").value.toString()
+                    )
+                }
+
+                setBuildingsSpinnerAdapter()
+                usersBranchListenerInit()
+            }
+        }
+
+        buildingsBranch = database.child("streets").child((streets.selectedItem as Street).id).child("buildings")
+        buildingsBranch.addValueEventListener(buildingsBranchListener!!)
+
+        setBuildingsSpinnerAction()
+    }
+
+    private fun setBuildingsSpinnerAdapter() {
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, buildingsList!!)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapter.notifyDataSetChanged()
+        buildings.adapter = adapter
+    }
+
+    private fun setBuildingsSpinnerAction() {
+        buildings.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Log.d("state", "nothing was selected in buildings")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                apartment.requestFocus()
+            }
+        }
+    }
+
+    private fun usersBranchListenerInit() {
+        usersBranchListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("state", p0.message)
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                selectUserInfo(
+                        merge(p0.child("name").value.toString(),
+                                p0.child("surname").value.toString()),
+                        p0.child("street_id").value.toString(),
+                        p0.child("building_id").value.toString(),
+                        p0.child("apartment").value.toString()
+                )
+            }
+
+        }
+
+        usersBranch = database.child("users").child(mAuth.currentUser!!.uid)
+        usersBranch.addValueEventListener(usersBranchListener!!)
+    }
+
+    private fun selectUserInfo(name: String, streetId: String, buildingId: String, apartment: String) {
+        fullName.setText(name)
+        streets.setSelection(streetsList!!.indexOfFirst { it.id == streetId })
+        buildings.setSelection(buildingsList!!.indexOfFirst { it.id == buildingId })
+        this.apartment.setText(apartment)
+        email.setText(mAuth.currentUser!!.email)
+    }
+
+    private fun merge(name: String, surname: String): String =
+            "$name $surname"
 }
