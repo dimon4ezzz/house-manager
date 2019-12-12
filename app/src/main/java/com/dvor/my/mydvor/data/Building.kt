a7de38fc682cc9bbd05be0f33f7c9a5386c76a94
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2019-12-12 23:03:54 +0500

    override toString in Street and Building

--- app/src/main/java/com/dvor/my/mydvor/data/Building.kt
+++ app/src/main/java/com/dvor/my/mydvor/data/Building.kt
@@ -3,4 +3,13 @@ package com.dvor.my.mydvor.data
 data class Building(
         val id: String,
         var number: String
-)
+) {
+    /**
+     * It is for spinners.
+     *
+     * @see com.dvor.my.mydvor.RegistrationActivity
+     * @see com.dvor.my.mydvor.settings.SettingsFragment
+     */
+    override fun toString(): String =
+            number
+}
